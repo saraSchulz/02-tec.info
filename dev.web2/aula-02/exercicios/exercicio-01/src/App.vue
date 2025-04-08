f89deacbf28472836d502ecb8d9343313f7d3abe
saraSchulz <saraschulz2805@gmail.com>
2025-04-08 15:36:48 -0300

    exercicio aula2 completo, fazer aula 3 e 4

--- dev.web2/aula-02/exercicios/exercicio-01/src/App.vue
+++ dev.web2/aula-02/exercicios/exercicio-01/src/App.vue
@@ -1,11 +1,56 @@
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    }
+  },
+  methods: {
+    incrementar() {
+      this.contador++
+    },
+    decrementar() {
+      if (this.contador > 0) {
+        this.contador--
+      }
+    },
+    reiniciar() {
+      this.contador = 0
+    },
+  },
+}
 </script>
 
 <template>
-  <h1>TERMINAR</h1>
+  <h1>Contador</h1>
+  <p>{{ contador }}</p>
+  <div class="buttons">
+    <button @click="incrementar">➕</button>
+    <button @click="reiniciar">🔄</button>
+    <button @click="decrementar">➖</button>
+  </div>
 </template>
 
-<style scoped>
-
+<style>
+h1 {
+  color: #008b8b;
+  font-size: 3rem;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  margin: 2vw;
+}
+p {
+  margin: 2vw;
+  color: #ffffff;
+  font-size: 3vw;
+}
+div.buttons{
+  display: flex;
+}
+button {
+  border: black;
+  padding: 5px;
+  background-color: black;
+  margin: 10px;
+  border-radius: 10px;
+}
 </style>
---
+++ dev.web2/aula-04/README.md
@@ -0,0 +1,5 @@
+# 02-tec.info
+______________________
+2 ano 2INFO2
+
+DESENVOLVIMENTO WEB
