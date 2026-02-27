cc5266aff569277c7334aa3dd893dd0c6a7e3248
saraSchulz <sara.ifc2024@gmail.com>
2026-02-27 11:17:10 -0300

    Aula Dev.web 27/02

---
+++ dev.web2/Vue-02_Diretivas/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'
+const text = ref('');
+
+//v-if (renderização condicional), v-for (listagem de elementos) e v-model (vinculação bidirecional de dados)
+</script>
+<template>
+    <div class="container">
+        <p>Diretivas-vue</p>
+        <input type="text" v-model="text">
+        <p>O texto é {{ text }} ( Qauntidade de Cacacteres: {{ text.length }})</p>
+    </div>
+</template>
+
+<style scoped>
+</style>
