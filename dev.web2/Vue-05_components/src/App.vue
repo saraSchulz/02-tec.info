12c2e86bd7b73a097f30790d027e99709e9d0be9
saraSchulz-art <sara.ifc2024@gmail.com>
2026-04-07 16:53:55 -0300

    componentização init

---
+++ dev.web2/Vue-05_components/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from 'vue'
+import ButtonChild from './components/buttonChild.vue'
+let acoes = ref([
+  { id: 1, nome: 'Novo', tipo: 'novo' },
+  { id: 2, nome: 'Editar', tipo: 'editar' },
+  { id: 3, nome: 'Salvar', tipo: 'salvar' },
+  { id: 4, nome: 'Cancelar', tipo: 'cancelar' },
+  { id: 5, nome: 'Excluir', tipo: 'excluir' },
+]);
+
+function clickButton(mensagem) {
+  alert(`Você clicou no botão ${mensagem}!`);
+}
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <buttonChild v-for="acao in acoes"
+      :key="acao.id"
+      :nome="acao.nome"
+      :tipo="acao.tipo"
+      estilo='btn'
+      @click="clickButton"
+      >
+        {{acao.nome}}
+      </buttonChild>
+    </div>
+  </div>
+  <div class="lista">
+    
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ dev.web2/Vue-05_components/src/components/buttonChild.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps(['nome', 'tipo', 'estilo'])
+
+defineEmits(['click'])
+</script>
+<template>
+
+  <button :class="[estilo, tipo]"
+    @click.prevent="$emit('click', nome)"
+  >
+    <slot></slot>
+  </button>
+
+</template>
+<style scoped>
+.btn {
+  color: black;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.btn.salvar {
+  background-color: mediumspringgreen;
+}
+
+.btn.editar {
+  background-color: yellow;
+}
+
+.btn.cancelar {
+  background-color: orange;
+}
+
+.btn.excluir {
+  background-color: red;
+}
+
+.btn.novo {
+  background-color: lawngreen;
+
+}
+</style>
